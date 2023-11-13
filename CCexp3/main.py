17e955fc99f9aa0ab08260a975a7e0fa2bfa86e3
msdkabil <142490140+msdkabil@users.noreply.github.com>
2023-11-13 16:21:18 +0530

    Update main.py

--- CCexp3/main.py
+++ CCexp3/main.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-"""Return a friendly HTTP greeting."""
-return 'Hello Kabilan!'
+  """Return a friendly HTTP greeting."""
+  return 'Hello Kabilan!'
 if __name__ == '__main__':
-app.run(host='127.0.0.1', port=8080, debug=True)
+  app.run(host='127.0.0.1', port=8080, debug=True)
