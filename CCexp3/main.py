f17d749bb51e795a781b8faf9a171ea922ca154c
msdkabil <142490140+msdkabil@users.noreply.github.com>
2023-11-13 15:51:04 +0530

    Update main.py

--- CCexp3/main.py
+++ CCexp3/main.py
@@ -1,24 +1,9 @@
-from flask import Flask, render_template, request, abort, Response
 
+from flask import Flask
 app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-
-def get_weather():
-    city = request.args.get('city')
-    if city is None:
-        abort(400, 'Missing argument city')
-    data = {}
-    data['q'] = request.args.get('city')
-    data['appid'] = 'XXXXXXXXX'
-    data['units'] = 'metric'
-    url_values = urllib.parse.urlencode(data)
-    url = 'http://api.openweathermap.org/data/2.5/forecast'
-    full_url = url + '?' + url_values
-    data = urllib.request.urlopen(full_url)
-    resp = Response(data)
-    resp.status_code = 200
-    return render_template('index.html', title='Weather App', data=json.loads(data.read().decode('utf8')))
-
+@app.route('/')
+def hello():
+"""Return a friendly HTTP greeting."""
+return 'Hello Kabilan!'
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+app.run(host='127.0.0.1', port=8080, debug=True)
