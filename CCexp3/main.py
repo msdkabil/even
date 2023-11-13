ddd79444281a876ae24455c102fe98a2e6113327
msdkabil <142490140+msdkabil@users.noreply.github.com>
2023-11-13 16:43:48 +0530

    Update main.py

--- CCexp3/main.py
+++ CCexp3/main.py
@@ -1,9 +1,21 @@
+from flask import Flask, request, render_template
+import requests
 
-from flask import Flask
 app = Flask(__name__)
-@app.route('/')
-def hello():
-  """Return a friendly HTTP greeting."""
-  return 'Hello Kabilan!'
-if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8080, debug=True)
+  
+@app.route('/', methods =["GET", "POST"])
+def index(): 
+    weatherData = ''
+    error = 0
+    cityName = ''
+    if request.method == "POST":       
+        cityName = request.form.get("cityName")  
+        if cityName:
+            weatherApiKey = '5b0b63b0baa793a645b839453e9a5b6d'
+            url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=" + weatherApiKey
+            weatherData = requests.get(url).json()
+        else:
+            error = 1    
+    return render_template('index.html', data = weatherData, cityName = cityName, error = error)
+if __name__ == "__main__":
+    app.run()
