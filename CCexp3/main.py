165d8f4fa64d635e66706abdbfa16ee7e381054a
msdkabil <142490140+msdkabil@users.noreply.github.com>
2023-11-20 09:46:02 +0530

    Update main.py

--- CCexp3/main.py
+++ CCexp3/main.py
@@ -5,17 +5,10 @@ app = Flask(__name__)
   
 @app.route('/', methods =["GET", "POST"])
 def index(): 
-    weatherData = ''
-    error = 0
-    cityName = ''
-    if request.method == "POST":       
-        cityName = request.form.get("cityName")  
-        if cityName:
-            weatherApiKey = '5b0b63b0baa793a645b839453e9a5b6d'
-            url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=" + weatherApiKey
-            weatherData = requests.get(url).json()
-        else:
-            error = 1    
-    return render_template('index.html', data = weatherData, cityName = cityName, error = error)
+    n=int(input("Enter the value of n:")
+    for(i in range(1,2n+1):
+        if(i%2==0)
+          print(i,end=" ")
+
 if __name__ == "__main__":
     app.run()
